2e09e0cccb403d9b17c23662d2c0b32e1c6e704c
AnonimousMex <AnonimousMex>
2024-10-19 19:26:23 -0600

    tarea

--- index.js
+++ index.js
@@ -1,126 +1,42 @@
-const input = document.getElementById("todo-input")
-const button = document.getElementById("add-btn")
-const todoList = document.getElementById("todo-list") 
-const hourHtml = document.getElementById("hours")
-const minutesHtml = document.getElementById("minutes")
-const secondsHtml = document.getElementById("seconds")
-const dayHtml = document.getElementById("day")
-const monthHtml = document.getElementById("month")
-const yearHtml = document.getElementById("year")
+const cardsContainer = document.querySelector('.cards-container');
 
+const createCard = (character) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-const validateInput = (todoItemText) => {
-    if (todoItemText.trim() === "") {
-        alert("No puedes registrar un ToDo Vacío");
-        return false;
+    card.innerHTML = `
+        <div class="left">
+            <img id="logo" src="${character.image}" alt="${character.name}">
+        </div>
+        <div class="right">
+            <div class="upper">
+                <h1 id="name">${character.name}</h1>
+                <div class="status-container">
+                    <div class="circle ${character.status.toLowerCase()}"></div>
+                    <p id="status">${character.status}</p>
+                </div>
+            </div>
+            <div class="middle">
+                <p class="gray">Last known location:</p>
+                <p id="location">${character.location.name}</p>
+            </div>
+            <div class="bottom">
+                <p class="gray">First seen in</p>
+                <p id="firstSeen">${character.origin.name}</p>
+            </div>
+        </div>
+    `;
+
+    cardsContainer.appendChild(card);
+};
+
+const fetchApiData = async () => {
+    for (let i = 0; i < 10; i++) {
+        const random = Math.floor(Math.random() * 827);
+        const data = await fetch("https://rickandmortyapi.com/api/character/" + random);
+        const character = await data.json();
+        createCard(character);
     }
-    return true;
-}
-
-
-const editTodo = (spanElement, liElement, flags) => {
-    spanElement.addEventListener("dblclick", () => {
-        const inputItem = document.createElement("input");
-        inputItem.type = "text";
-        inputItem.value = spanElement.textContent;
-        liElement.replaceChild(inputItem, spanElement);
-
-        inputItem.addEventListener("keypress", (event) => {
-            if (flags.completed === true) {
-                alert("No puedes editar un campo completado");
-            } else {
-                if (event.key === "Enter") {
-                    if (!validateInput(inputItem.value)) {
-                        return;
-                    }
-                    spanElement.textContent = inputItem.value;
-                    liElement.replaceChild(spanElement, inputItem);
-                }
-            }
-        });
-    });
 };
 
-
-const appendElementToList = (todoItemText, todoItemhour, todoItemDate) => {
-    const element = document.createElement("li");
-    const text = document.createElement("span");
-    const hour = document.createElement("span");
-    const date = document.createElement("span");
-    const div = document.createElement("div");
-    const deleteButton = document.createElement("button");
-    const completeButton = document.createElement("button");
-
-    const flags = { completed: false };
-
-    completeButton.textContent = "👍";
-    deleteButton.textContent = "Eliminar";
-    text.textContent = todoItemText;
-    hour.textContent = ` ${todoItemhour}`;
-    date.textContent = ` ${todoItemDate}`;
-
-    element.appendChild(text);
-    element.appendChild(hour);
-    element.appendChild(date);
-    div.appendChild(deleteButton);
-    div.appendChild(completeButton);
-    element.appendChild(div);
-
-    completeButton.addEventListener("click", () => {
-        element.classList.toggle("completed");
-        flags.completed = !flags.completed;
-    });
-
-    deleteButton.addEventListener("click", () => {
-        element.remove();
-    });
-
-    editTodo(text, element, flags); 
-
-    todoList.appendChild(element);
-    input.value = "";
-};
-
-
-const updateClock = () => {
-    const dateNow = new Date();
-    const hour = String(dateNow.getHours()).padStart(2, '0');
-    const minutes = String(dateNow.getMinutes()).padStart(2, '0');
-    const seconds = String(dateNow.getSeconds()).padStart(2, '0');
-
-    hourHtml.textContent = hour;
-    minutesHtml.textContent = minutes;
-    secondsHtml.textContent = seconds;
-}
-
-
-setInterval(updateClock, 1000);
-
-const updateDate = () => {
-    const now = new Date();
-    const day = now.getDate().toString().padStart(2, '0');
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const year = now.getFullYear();
-
-    dayHtml.textContent = day;
-    monthHtml.textContent = month;
-    yearHtml.textContent = year;
-};
-
-window.onload = function() {
-    updateDate();
-  };
-
-
-
-const addTodo = () => {
-    const time = `${hourHtml.textContent}:${minutesHtml.textContent}:${secondsHtml.textContent}`;
-    const date = `${dayHtml.textContent}/${monthHtml.textContent}/${yearHtml.textContent}`
-    const todoItemText = input.value;
-
-    if (!validateInput(todoItemText)) return;
-
-    appendElementToList(todoItemText, time, date);
-}
-
-button.addEventListener("click", addTodo);
+fetchApiData();
